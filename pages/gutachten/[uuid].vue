a60235afc3c26b74513134d9aaf47507892cdc0b
WolfsCoding <joshuanoel.boehmer@gmail.com>
2024-05-09 22:13:15 +0200

    Removed bg-muted

--- pages/gutachten/[uuid].vue
+++ pages/gutachten/[uuid].vue
@@ -102,7 +102,7 @@ function saveDetails() {
 </script>
 
 <template>
-    <div class="flex min-h-screen w-full flex-col bg-muted/40">
+    <div class="flex min-h-screen w-full flex-col">
         <div class="flex flex-col sm:gap-4 sm:py-4">
             <header class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
                 <Breadcrumb class="hidden md:flex">
