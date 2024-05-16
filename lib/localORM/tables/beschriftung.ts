53e7253cee2ee28b515560a126887ef2ec2515dd
WolfsCoding <joshuanoel.boehmer@gmail.com>
2024-05-16 15:09:13 +0200

    Added Beschriftung as ORM Class

---
+++ lib/localORM/tables/beschriftung.ts
@@ -0,0 +1,64 @@
+import { TextBuilder } from "@/lib/utils";
+import { BaseEntry } from "../BaseEntry";
+import { useToast } from "@/components/ui/toast";
+
+export class Beschriftung extends BaseEntry {
+    datum: string = "";
+    uhrzeit: string = "";
+    ort: string = "";
+    akz: string = "";
+    type: T_TYPES = TYPES.Unbekannt;
+
+    constructor(data: Partial<Beschriftung>) {
+        super(data);
+        this.datum = data.datum || "";
+        this.uhrzeit = data.uhrzeit || "";
+        this.ort = data.ort || "";
+        this.akz = data.akz || "";
+        this.type = data.type || TYPES.Unbekannt;
+    }
+
+    copyToClipboard() {
+        let text = "";
+
+        text += this.datum;
+        text += " - ";
+        text += this.uhrzeit;
+        text += " - ";
+        text += this.ort;
+
+        if (this.akz != "") {
+            text += " - ";
+            text += this.akz;
+        }
+
+        switch (this.type) {
+            case TYPES.Schussgefächt:
+                text += " - ";
+                text += "SG";
+                break;
+            case TYPES.Messerstecherrei:
+                text += " - ";
+                text += "MS";
+                break;
+            default:
+                break;
+        }
+
+        new TextBuilder().addLine(text).copyClipboard();
+
+        useToast().toast({
+            title: "Beschriftung kopiert",
+            description: "Die Beschriftung wurde in deine Zwischenablage kopiert.",
+        });
+    }
+}
+
+export const TYPES = {
+    Unbekannt: "Unbekannt",
+    Schussgefächt: "Schussgefächt",
+    Messerstecherrei: "Messerstecherrei",
+    Eigntumsdelikt: "Eigentumdelikt",
+} as const;
+
+export type T_TYPES = (typeof TYPES)[keyof typeof TYPES];
