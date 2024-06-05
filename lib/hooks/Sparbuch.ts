7f2f57b31429bf6b9cdc3b89cfaf0dbc23e8d910
WolfsCoding <joshuanoel.boehmer@gmail.com>
2024-06-05 20:18:06 +0200

    Fix: Rounded Sparbuch amount

--- lib/hooks/Sparbuch.ts
+++ lib/hooks/Sparbuch.ts
@@ -1,48 +1,53 @@
-import { toast } from "vue-sonner";
-import { LocalStorage } from "../localORM";
-import { ENTRY_TYPES, SparbuchEntry } from "../localORM/tables/sparbuch";
+import { toast } from 'vue-sonner';
+import { LocalStorage } from '../localORM';
+import { ENTRY_TYPES, SparbuchEntry } from '../localORM/tables/sparbuch';
 
-const sparbuchDB = new LocalStorage<SparbuchEntry>("sparbuch", (data: any) => new SparbuchEntry(data));
+const sparbuchDB = new LocalStorage<SparbuchEntry>(
+  'sparbuch',
+  (data: any) => new SparbuchEntry(data)
+);
 const sparbuchEntrys: Ref<SparbuchEntry[]> = ref(await sparbuchDB.getAll());
 
 function getSparbuchAmount(): ComputedRef<number> {
-    return computed((): number => {
-        let amount = 0;
+  return computed((): number => {
+    let amount = 0;
 
-        for (const entry of sparbuchEntrys.value) {
-            if (entry.type == ENTRY_TYPES.Einzahlung) {
-                amount += entry.betrag;
-            } else if (entry.type == ENTRY_TYPES.Auszahlung) {
-                amount -= entry.betrag;
-            } else if (entry.type == ENTRY_TYPES.Zinsen) {
-                amount += entry.betrag;
-            }
-        }
+    for (const entry of sparbuchEntrys.value) {
+      if (entry.type == ENTRY_TYPES.Einzahlung) {
+        amount += entry.betrag;
+      } else if (entry.type == ENTRY_TYPES.Auszahlung) {
+        amount -= entry.betrag;
+      } else if (entry.type == ENTRY_TYPES.Zinsen) {
+        amount += entry.betrag;
+      }
 
-        return amount;
-    });
+      amount = Math.round(amount * 100) / 100;
+    }
+
+    return amount;
+  });
 }
 
 async function addSparbuchEntry(entry: SparbuchEntry) {
-    sparbuchDB.add(entry);
-    sparbuchEntrys.value = await sparbuchDB.getAll();
+  sparbuchDB.add(entry);
+  sparbuchEntrys.value = await sparbuchDB.getAll();
 }
 
 async function removeSparbuchEntry(id: string) {
-    sparbuchDB.delete(id);
+  sparbuchDB.delete(id);
 
-    sparbuchEntrys.value = await sparbuchDB.getAll();
+  sparbuchEntrys.value = await sparbuchDB.getAll();
 
-    toast("Erfolgreich gelöscht", {
-        description: "Der Eintrag wurde erfolgreich gelöscht",
-    });
+  toast('Erfolgreich gelöscht', {
+    description: 'Der Eintrag wurde erfolgreich gelöscht',
+  });
 }
 
 export function useSparbuch() {
-    return {
-        removeSparbuchEntry,
-        addSparbuchEntry,
-        sparbuchEntrys,
-        getSparbuchAmount,
-    };
+  return {
+    removeSparbuchEntry,
+    addSparbuchEntry,
+    sparbuchEntrys,
+    getSparbuchAmount,
+  };
 }
